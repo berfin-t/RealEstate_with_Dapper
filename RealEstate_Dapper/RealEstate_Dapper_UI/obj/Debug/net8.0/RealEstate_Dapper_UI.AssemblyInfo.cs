1d221f8b2928c9968ce02cafe407dd2941a6ab43
Berfin Tek <69851179+berfin-t@users.noreply.github.com>
2024-08-24 00:05:11 +0300

    All "partial component" operations have been completed in UI

--- RealEstate_Dapper/RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
+++ RealEstate_Dapper/RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b98b231ed93bf282d60709ba5f8f50d7aef599")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254824eef3aa907f73a482280d733af3a92946f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
